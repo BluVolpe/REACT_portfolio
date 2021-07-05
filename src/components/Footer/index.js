c19db8d3e148bb4295e724f67b99dea398985940
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-07-05 14:41:55 -0400

    css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,11 @@ import './footer.css';
 function Footer() {
   return (
     <footer>
-        <h2 id="contact">Contact Me: </h2>
-        <a href="https://github.com/BluVolpe">___GITHUB___</a>
-        <a href="https://www.linkedin.com/in/david-volpe-54b5b9133/">___LINKEDIN___</a>
+        <h2 id="contact">Contact Me</h2>
+        <div className="footerLinks">
+        <a href="https://github.com/BluVolpe">GitHUB</a>
+        <a href="https://www.linkedin.com/in/david-volpe-54b5b9133/">LINKEDIN</a>
+        </div>
     </footer>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,10 @@ function Header() {
     <header>
       <h1 className="myName">David Volpe</h1>
       <nav>
-        <a href="#about">About Me</a>
-        <a href="#work">Work</a>
-        <a href="#contact">Contact Me</a>
-        <a href="https://docs.google.com/document/d/1_mcMP2gXawqt3XsfGJbXuEi_mga_-1Evwz8h0Za9lHU/edit#heading=h.y7d3xdxnr44m">Resume</a>
+        <a href="#about" className="navLinks">About Me</a>
+        <a href="#work" className="navLinks">Work</a>
+        <a href="#contact" className="navLinks">Contact Me</a>
+        <a href="https://docs.google.com/document/d/1_mcMP2gXawqt3XsfGJbXuEi_mga_-1Evwz8h0Za9lHU/edit#heading=h.y7d3xdxnr44m" className="navLinks">Resume</a>
       </nav>
     </header>
   );
